64bb88e5cf82bf9e0cedefa70860252b96318528
Luki L <luki97r@gmail.com>
2025-01-27 21:22:42 +0100

    2 sets of letters choice

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template_string, url_for, request, render_template
 from unidecode import unidecode
-from checks import check, generate_results_html
+from checks import generate_sets, check, generate_results_html
 
 app = Flask(__name__)
 
@@ -9,22 +9,27 @@ app = Flask(__name__)
 def start_page():
    return render_template('input.html')
 
-@app.route('/input', methods=['POST', 'GET'])
+@app.route('/input', methods=['POST', 'GET']) # moze tylko POST? i bez else
 def input_page():
     if request.method == 'POST':
         input_data = request.form['chars']
+        selected = request.form.getlist('zestaw')
     else:
         input_data = request.args.get('chars')
     input_data = unidecode(input_data.upper())
-    return redirect(url_for('result_page', letters=input_data))
+    # to do: po minusie mozna dac zgubione literki, np luca di bosqo
+    sets = generate_sets(selected)
+    print("input_page", selected, sets)
+    return redirect(url_for('result_page', letters=input_data, sets=sets))
 
 # @app.route('/example')
 # def example_result_page():
 #    return render_template('example_result.html')
 
-@app.route('/result/<letters>')
-def result_page(letters):
-    checked, left = check(letters)
+@app.route('/result/<letters>::<sets>')
+def result_page(letters, sets):
+    print("result_page", sets)
+    checked, left = check(letters, sets)
     return render_template_string(generate_results_html(letters, checked, left))
 
 if __name__ == '__main__':
--- checks.py
+++ checks.py
@@ -1,11 +1,24 @@
 from airium import Airium
 
-def check(inscription):
-    set1 = "EKANBBBDR#!IAFFGGGWYADZILN4GEERAPPPQCOOXOORUJTMVVCWVEXANNZKO#LHEJJESTESTUSAMA?"
-    # my cap with 'LUCADIBOSCO' was stolen, I bought a second one
-    set2 = set1.replace("L", "",1).replace("U", "",1).replace("C", "",2).replace("A", "",1).replace("D", "",1).replace("I", "",1).replace("B", "",1).replace("O", "",2).replace("S", "",1)
-    set = (set1+set2).replace("C", "Q",1) # on stolen cap i have Q repainted to C
-    alphabet = "".join(sorted(set))
+standard_set = "EKANBBBDR#!IAFFGGGWYADZILN4GEERAPPPQCOOXOORUJTMVVCWVEXANNZKO#LHEJJESTESTUSAMA?"
+
+strings_dict = {
+    "standard": standard_set,
+    "luca_di_bosqo": standard_set.replace("L", "",1).replace("U", "",1).replace("C", "",1).replace("A", "",1).replace("D", "",1).replace("I", "",1).replace("B", "",1).replace("O", "",2).replace("S", "",1).replace("Q", "",1),
+
+}
+
+def generate_sets(selected):
+    output_str = ""
+    for key in selected:
+        if key in strings_dict:
+            output_str += strings_dict[key]
+        else:
+            print("No key {} in hardcoded sets of letters", key)
+    print("generate_sets", selected, output_str)
+    return "".join(sorted(output_str))
+
+def check(inscription, alphabet):
     result = [True]*len(inscription)
 
     for idx, char in enumerate(inscription):
@@ -39,7 +52,7 @@ def generate_results_html(letters, checked, left):
                 else:
                     color="red"
                 char = letters[i]
-                if char is " ":
+                if char == " ":
                     char="&MediumSpace;"
                 with html.span(style="font-family:verdana;color:"+color+";"):
                     html(char)
